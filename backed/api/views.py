940c2182f902c2c74f1aebda1629962a6e68e7bc
doody <baazize-khadija@hotmail.com>
2025-10-03 09:03:03 +0200

    REST API with a simple client

---
+++ backed/api/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+def api_view(request, *args, **kwargs):
+    data = {
+        'name': 'dody',
+        'language': 'python'
+    }
+
+    return JsonResponse(data)
+
